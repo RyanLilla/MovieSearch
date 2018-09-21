f30e4a2c911e5f69200dc2f0650eac85d0d51405
RyanLilla <r.lilla1026@gmail.com>
2018-09-21 10:18:51 -0700

    Added files via upload & set up project

---
+++ MovieSearch/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace MovieSearch
+{
+    public class Movie
+    {
+        public string ID { get; set; }
+        public string TitleType { get; set; }
+        public string Title { get; set; }
+        //public int YearReleased { get; set; }
+        public string YearReleased { get; set; }
+        public string Runtime { get; set; }
+        public string Genre { get; set; }
+        
+
+        internal static Movie ParseFromTSV(string line)
+        {
+            var columns = line.Split('\t');
+            //var yearColumn = Int32.TryParse(columns[5], out int year);
+
+            return new Movie
+            {
+                ID = columns[0],
+                TitleType = columns[1],
+                Title = columns[2],
+                YearReleased = columns[5],
+                //YearReleased = year,
+                Runtime = columns[7],
+                Genre = columns[8]
+            };
+        }
+    }
+}
---
+++ MovieSearch/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MovieSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movies_basics = ProcessBasicsFile(@"title.basics.tsv");
+            //var movies_basics = ProcessBasicsFile(@"title.basics - Copy.tsv");
+            var movies_akas = ProcessAkasFile(@"title.akas.tsv");
+            
+            var movieQuery =
+                from movie in movies_basics
+                join extras in movies_akas
+                on movie.Title equals extras.Title
+                where movie.TitleType != "tvEpisode" && extras.Region == "US" && movie.YearReleased == "2018"
+                select new
+                {
+                    movie.ID,
+                    movie.Title,
+                    movie.YearReleased,
+                    movie.Genre,
+                    extras.Region,
+                    extras.Language
+                };
+
+            var resultsQuery = movieQuery
+                .GroupBy(movie => movie.Title)
+                .Select(g => g.OrderBy(movie => movie.Title).First());
+
+            Console.WriteLine($"The total number of results in the search: {resultsQuery.Count()}");
+
+            foreach (var movie in resultsQuery.Take(30))
+            {
+                if (movie.YearReleased == @"\N")
+                    Console.WriteLine($"Movie: {movie.Title} -- Year Released: N/A -- Region: {movie.Region}");
+                else
+                    Console.WriteLine($"Movie: {movie.Title} -- Year Released: {movie.YearReleased} -- Region: {movie.Region}");
+            }
+        }
+
+        static List<Movie> ProcessBasicsFile(string path)
+        {
+            Console.WriteLine("Processing...");
+            Console.WriteLine();
+
+            var query = from line in File.ReadLines(path).Skip(1)
+                        where line.Length > 1
+                        select Movie.ParseFromTSV(line);
+
+            return query.ToList();
+        }
+
+        static List<MovieExtras> ProcessAkasFile(string path)
+        {
+            var query = from line in File.ReadLines(path).Skip(1)
+                        where line.Length > 1
+                        select MovieExtras.ParseFromTSV(line);
+
+            return query.ToList();
+
+        }
+    }
+}
